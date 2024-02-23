5d4c9d05aa9438d6726be4aa676631c8a87b410d
Devika-723 <131938919+Devika-723@users.noreply.github.com>
2024-02-23 11:09:05 +0530

    Add files via upload

---
+++ enquiry.js
@@ -0,0 +1,9 @@
+function getResponse(input){
+    if (input == "college duration"){
+        return "9.00 am to 4.40 pm";
+    }
+    if (input=="courses"){
+        return "Engineering UG and PG, MBA and MCA";
+        
+    }
+}
---
+++ responses.js
@@ -0,0 +1,65 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "show anitha details") {
+        return "Name:R.Anitha,Rollno:4001,Branch:CSE, Sem:6"; } 
+        else if (input == "no name") {
+        return "No Name";
+    } else if (input == "show devika details") {
+        return "Name:R.Devika, Rollno:4008,Branch:CSE,Sem:6";
+    }
+ 
+    if (input == "show anjana details") {
+        return "Name:R.Anjana,  Rollno:4002 Branch:CSE Sem:6";}
+        else if (input == "no name") {
+        return "No Name";
+    } else if (input == "show kaviya details") {
+        return "Name:S.Kaviya,Rollno:4018,Branch:CSE,Sem:6";
+    }
+    if (input == "show arularasi details") {
+        return "Name:S.Arularasa9,  Rollno:4003 Branch:CSE Sem:6";}
+        else if (input == "no name") {
+        return "No Name";
+    } else if (input == "show arun details") {
+        return "Name:S.Arun,Rollno:4004,Branch:CSE,Sem:6";
+    }
+    if (input == "show aysha sumaya details") {
+        return "Name:J.Aysha sumaya,  Rollno:4005 Branch:CSE Sem:6";}
+        else if (input == "no name") {
+        return "No Name";
+    } else if (input == "no name") {
+        return "No Name";
+    }
+    if (input == "show bose details") {
+        return "Name:V.Bose,  Rollno:4007 Branch:CSE Sem:6";}
+        else if (input == "show balaji details") {
+        return "Name:M.Balaji,Rollno:4006,Branch:CSE,Sem:6";
+    } else if (input == "show dharshni details") {
+        return "Name:S.Dharshni,Rollno:4009,Branch:CSE,Sem:6";
+        
+    }
+    if (input == "college duration"){
+        return "9.00 am to 4.40 pm";
+    }
+    if (input=="courses"){
+        return "Engineering UG and PG, MBA and MCA";
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+    // Simple responses
+    if (input == "hi") {
+        return "Hello there!";
+    } else if (input == "bye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
